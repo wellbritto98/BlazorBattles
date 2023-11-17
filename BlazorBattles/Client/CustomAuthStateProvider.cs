f1fca9a9fa9a55a23ed9f85d90fac8e85701bda3
José Wellington <josewellington@infowsoftware.com.br>
2023-11-17 18:19:23 -0300

    navmenu

---
+++ BlazorBattles/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Claims;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorBattles.Client;
+
+public class CustomAuthStateProvider : AuthenticationStateProvider
+{
+    private readonly ILocalStorageService _localStorageService;
+
+    public CustomAuthStateProvider(ILocalStorageService localStorageService)
+    {
+        _localStorageService = localStorageService;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var state = new AuthenticationState(new ClaimsPrincipal());
+        
+        
+        if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
+        {
+            var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "John"),
+                },
+                "test authentication type");
+
+            var user = new ClaimsPrincipal(identity);
+
+            state = new AuthenticationState(user);
+            
+        }
+        NotifyAuthenticationStateChanged(Task.FromResult(state));
+        return state;
+    }
+}
--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -1,11 +1,25 @@
 using BlazorBattles.Client;
+using BlazorBattles.Client.Services;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.Toast;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IBananaService, BananaService>();
+builder.Services.AddScoped<IUnitService, UnitService>();
+builder.Services.AddBlazoredToast();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddBlazoredLocalStorage();
+
+
 
 await builder.Build().RunAsync();
